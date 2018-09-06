b894b34b74cfde62c6db4756a167563334036e97
Ignacio R Espino <irespino@capozios.com>
2018-09-06 23:16:56 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,6 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |key, value|
     puts "#{key.capitalize}:"
     value.each do |key, value|
+      key_array = key.split("_")
       puts "#{key.capitalize}: #{value}"
     end
   end
