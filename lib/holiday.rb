4c2990fecf162272f182902a4c3da05da6e80539
Ignacio R Espino <irespino@capozios.com>
2018-09-06 22:40:22 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,9 +62,10 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |key, value|
-    key[0] = key[0].
+    key[0] = key[0].upcase
     puts key
     value.each do |key, value|
+      key[0] = key[0].upcase
       puts "#{key} #{value}"
     end
   end
