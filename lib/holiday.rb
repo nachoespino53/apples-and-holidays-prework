547916da6e30ac0968673930ef612f45c824108d
Ignacio R Espino <irespino@capozios.com>
2018-09-06 23:08:09 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,22 +74,22 @@ def all_holidays_with_bbq(holiday_hash)
 
 end
 
-# holiday_hash =
-#     {
-#       :winter => {
-#         :christmas => ["Lights", "Wreath"],
-#         :new_years => ["Party Hats"]
-#       },
-#       :summer => {
-#         :fourth_of_july => ["Fireworks", "BBQ"]
-#       },
-#       :fall => {
-#         :thanksgiving => ["Turkey"]
-#       },
-#       :spring => {
-#         :memorial_day => ["BBQ"]
-#       }
-#     }
+holiday_hash =
+    {
+      :winter => {
+        :christmas => ["Lights", "Wreath"],
+        :new_years => ["Party Hats"]
+      },
+      :summer => {
+        :fourth_of_july => ["Fireworks", "BBQ"]
+      },
+      :fall => {
+        :thanksgiving => ["Turkey"]
+      },
+      :spring => {
+        :memorial_day => ["BBQ"]
+      }
+    }
 
 
 
