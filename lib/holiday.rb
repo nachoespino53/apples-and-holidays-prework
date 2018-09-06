971dc2ef91aaaecfcbc1a3ffedaf0030920ec14c
Ignacio R Espino <irespino@capozios.com>
2018-09-06 23:40:16 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -92,7 +92,9 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.collect do |key, values|
-    
+    values.collect do |key, values|
+      if values.include?
+    end
   end
 end
 
